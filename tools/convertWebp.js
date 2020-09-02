907640e92c9d9d6d7319fe9265749f7596a3364f
Robert Smith <txresmith@gmail.com>
2020-09-01 22:16:09 -0500

    lowered quality on the webp conversions

--- tools/convertWebp.js
+++ tools/convertWebp.js
@@ -1,23 +1,23 @@
-const imagemin = require('imagemin')
-const imageminWebp = require('imagemin-webp')
-const glob = require('glob')
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+const glob = require("glob");
 
-;(async () => {
-  const searchPattern = `public/**/`
-  console.log(`convertWebp: searchPattern: ${searchPattern}`)
-  const blogs = glob.sync(searchPattern)
+(async () => {
+  const searchPattern = `public/**/`;
+  console.log(`convertWebp: searchPattern: ${searchPattern}`);
+  const blogs = glob.sync(searchPattern);
 
   //remove path and extension to leave filename only
   blogs.map(directory => {
-    const imageDirectory = `${directory}*.{jpg,png}`
+    const imageDirectory = `${directory}*.{jpg,png}`;
     // const toDirectory = directory.replace(Constants.markdownDir, 'public')
-    console.log(`converting directory: ${imageDirectory} to: ${directory}`)
+    console.log(`converting directory: ${imageDirectory} to: ${directory}`);
 
     imagemin([imageDirectory], {
       destination: directory,
-      plugins: [imageminWebp({ quality: 95, sharpness: 1, nearLossless: 80 })],
-    })
-  })
+      plugins: [imageminWebp({ quality: 75 })]
+    });
+  });
 
-  console.log('Images optimized')
-})()
+  console.log("Images optimized");
+})();
